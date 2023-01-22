53375d99b3e033bb0a4635f91ea4d1ec4703a3a4
KutsaevPavlo <p1ul@ukr.net>
2023-01-22 17:15:06 +0200

    update task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,12 +2,34 @@ const startBtn = document.querySelector('button[data-start]');
 
 // console.log(startBtn.textContent)
 
-const stopStn = document.querySelector('button[data-stop]');
+const stopBtn = document.querySelector('button[data-stop]');
 
-// console.log(stopStn.textContent)
+// console.log(stopBtn.textContent)
 
+const bodyEl = document.querySelector("body");
+// console.log(bodyEl.textContent)
+stopBtn.disabled = true;
 
+let timerId = null;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+
+
+
+  startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        bodyEl.style.backgroundColor  = color;
+    }, 1000);
+  });
+
+
+  stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+  });
