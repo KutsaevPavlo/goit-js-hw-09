3e5548aae5d726cecea962381390d5874716c17b
KutsaevPavlo <p1ul@ukr.net>
2023-01-22 16:39:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const startBtn = document.querySelector('button[data-start]');
 
+// console.log(startBtn.textContent)
+
+const stopStn = document.querySelector('button[data-stop]');
+
+// console.log(stopStn.textContent)
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
